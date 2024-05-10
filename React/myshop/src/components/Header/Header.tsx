fc605f290c3fe43cf9ee77e3742ded6101c79840
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-05-10 12:41:08 -0300

    deploy myshop vercel

--- React/myshop/src/components/Header/Header.tsx
+++ React/myshop/src/components/Header/Header.tsx
@@ -39,7 +39,7 @@ export const Header: React.FC = () => {
   return (
     <S.StyledHeader>
       <S.Wrapper>
-        <S.HeaderTitle>MyShop.</S.HeaderTitle>
+        <S.HeaderTitle>Shopping</S.HeaderTitle>
 
         <S.ButtonsWrapper>
           <S.AuthButton isLogged={isLogged} onClick={handleUserAuth}>
